04d49d088072d4f53947e4bc6dc4924449b03a87
Sequence-10 <habz3963@gmail.com>
2023-02-19 02:24:52 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -The function will print all letterws except q & e
+ *
+ *Return: all sucess except q&e
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+
+	return (0);
+}
