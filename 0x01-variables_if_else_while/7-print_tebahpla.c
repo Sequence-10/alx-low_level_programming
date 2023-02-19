af8e3a875c3a2abf61756d0f79072c8b01629666
Sequence-10 <habz3963@gmail.com>
2023-02-19 13:03:30 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -function to print reverse alphabets
+ *
+ * Return: output success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
