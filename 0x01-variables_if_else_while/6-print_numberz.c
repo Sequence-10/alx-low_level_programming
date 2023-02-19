78e42edffba54a71855a222296a674107e06e87b
Sequence-10 <habz3963@gmail.com>
2023-02-19 13:25:44 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -funtion that numbers with putchar
+ *
+ * Return: putchar to print numbers
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 48; q < 58; q++)
+		putchar(q);
+	putchar('\n');
+
+	return (0);
+}
