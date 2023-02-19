137c998e03fcf1d6f8c25af66c919830b690c970
Sequence-10 <habz3963@gmail.com>
2023-02-19 02:39:31 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * main - the function that prints all single digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+		printf("%d \n", m);
+
+	return (0);
+}
