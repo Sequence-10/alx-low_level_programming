88344a7e4a2bdb68b8f1b36006719d5730619157
Sequence-10 <habz3963@gmail.com>
2023-02-20 18:25:38 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -three digit number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48 + 1; b < 58; b++)
+		{
+			for (c = b + 1; c < 58; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+
+				if (a != 55 || b != 56 || c != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
