8332c2c329043b6931c3dec2d5629ad5dd9f5c4b
Sequence-10 <habz3963@gmail.com>
2023-02-20 16:52:49 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -singles numbers with sapec
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
