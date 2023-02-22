75700b1145266340f6484eba4ef36843096c686c
Sequence-10 <habz3963@gmail.com>
2023-02-21 17:57:11 -0800

    Task 0

---
+++ 0x02-funtions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * Jack_bauer - function that prints the time 
+ * Return: success
+ */
+
+void jack_bauer(void);
+{
+	int a, b;
+
+	a = 0;
+	while (a < 24)
+	{
+		b = 0;
+		while (b < 60)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
+		}
+		a++;
+	}
+}
