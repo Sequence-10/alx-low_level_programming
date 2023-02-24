781d8e0ac319c763d57b302cdf1edb53dedd2d4d
Sequence-10 <habz3963@gmail.com>
2023-02-23 18:26:23 -0800

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit -check for a digit
+ * @c: holds the digit
+ * Return: Success
+ */
+int _isdigit(int c);
+{
+	if (c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
