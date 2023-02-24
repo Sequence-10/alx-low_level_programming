74e5210e88b7f4e0619bddcb6688dc79e3376c86
Sequence-10 <habz3963@gmail.com>
2023-02-24 02:40:05 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,16 @@ void more_numbers(void)
 {
 	int i, j;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i <= 14; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10)
+			if (j > 9)
 			{
-			_putchar(j + '0')
+				_putchar((j / 10) + '0');
 			}
+			_putchar((j % 10) + '0');
 		}
-		i++
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
