20a9aabcfafdfe1434320a0df2ffe90b0c7d2964
Sequence-10 <habz3963@gmail.com>
2023-02-27 14:57:00 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - as it is said reset to 98
+ * @n: holds the return
+ * Retur: success
+ */
+void reset_to_98(int *n)
+{
+	int *n;
+	int a = 70;
+	*n = &a;
+	*n = 98;
+	_putchar(a);
+}
