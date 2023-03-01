beaff94a389999f8d9dab69621ef8ddba0f7f03a
Sequence-10 <habz3963@gmail.com>
2023-03-01 03:08:59 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,19 @@
 #include "main.h"
 /**
  * print_last_digit - get the last digit
- * @n: hold the last digit
  * Return: sucess
  */
 
-int print_last_digit(int n)
+int print_last_digit(int)
 {
 	int a;
+	int n;
 
-	if (n > 0)
+	if (n < 0)
 	{
 		n = -n;
 		a = n % 10;
-	}
-	if (a < 0)
-	{
-		a = -a;
-		_putchar(a = '0');
+		_putchar(a + '0');
 	}
 	return (a);
 }
