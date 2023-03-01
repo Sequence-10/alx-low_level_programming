723a2dcf9370f3f959d674ceb8dc67496c5bef25
Sequence-10 <habz3963@gmail.com>
2023-03-01 01:09:00 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes intege values
+ * @n: hold the value
+ * Return: suceess
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
