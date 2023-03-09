93c0c5cc745461befb3cf79d94c4d9dfbc5459b7
Sequence-10 <habz3963@gmail.com>
2023-03-08 17:09:32 -0800

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - print fibonacci
+ * Return: success
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 2;
+	long k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+		{
+			sum += k;
+			j = k - j;
+			++i;
+		}
+		printf("%ld\n", sum);
+		return (0);
+	}
+}
