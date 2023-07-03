4000de00b303e3d0afc0340c4b9e53d951ac99b7
Sequence-10 <habz3963@gmail.com>
2023-07-03 14:42:31 -0700

    Alx SE Foundation C-Language(Pointers, arrays & strings Task-3)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,43 @@
+#include "main.h"
+  
+/**
+ * _strncpy - function to copy
+ * @dest: word to copy
+ * @src: word to copy
+ * @n: word to copy
+ * Return: success
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+		if (n < b)
+		{
+			if (a < n)
+			{
+				dest[a] = src[a];
+			}
+		}
+		else
+		{
+			if (a < b)
+			{
+				dest[a] = src[a];
+			}
+			else if(a < n)
+			{
+				dest[a] = '\0';
+			}
+		}
+	}
+	for (b = 0; src[b] != '\0'; b++)
+	{
+		;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
