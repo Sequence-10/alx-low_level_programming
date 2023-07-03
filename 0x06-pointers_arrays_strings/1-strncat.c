5d20aef4bd751350e7da02da078687f5fa9122b5
Sequence-10 <habz3963@gmail.com>
2023-07-02 19:48:48 -0700

    Alx online program foundation level (pointers, arrays & strings TASK-2)

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - function to concatenate
+ * @dest: word to concat
+ * @src: word to concat
+ * @n: word to concat
+ * Return: success
+ */
+
+char *_strncat(char *dest, char *src. int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
