4a211af46e49bcc847623e8a825daa15357e404f
Sequence-10 <habz3963@gmail.com>
2023-07-02 19:40:46 -0700

    Alx online program foundation level (pointers, arrays & strings TASK-1)

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,16 +15,12 @@ char *_strcat(char *dest, char *src)
 		while (dest[a] != '\0')
 		{
 			a++;
-		
 		}
 		for (b = 0; src[b] != '\0'; b++)
 		{
 			dest[a] = src[b];
 			a++;
-		
 		}
 		dest[a] = '\0';
 		return (dest);
-	
-	
 	}
