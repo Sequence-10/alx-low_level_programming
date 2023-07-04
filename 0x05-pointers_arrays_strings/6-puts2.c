546b80bd58059a37c6ac421bca01001f43f61aa6
Sequence-10 <habz3963@gmail.com>
2023-07-03 20:18:51 -0700

    Alx SE Foundation Program On C-Language (Intro To pointers, Strings & Arrays Tasks-6/7/8/9/10/11)

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: the string
+ */
+void puts2(char *str)
+{
+	int i = 0, j;
+
+	while (str[i])
+		i++;
+	for (j = 0; j < i; j += 2)
+		_putchar(str[j]);
+
+	_putchar('\n');
+}
