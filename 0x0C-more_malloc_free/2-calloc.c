1953d54b92b47becc2d5b9888b85d2419469cd0e
Sequence-10 <habz3963@gmail.com>
2023-07-14 16:19:40 -0700

    Foundation Level - C Language Module - MORE-ON-MALLOC-FREE (Task-2)

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_calloc - validate the process
+ * @nmemb: container for the first input
+ * @size: variable handler
+ * return: success
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, j = 0;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	j = nmemb * size;
+	ptr = malloc(j);
+
+	if (ptr == NULL)
+		return (NULL);
+	while (i < j)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
