48f4069e7be0c4d33d915b97355322841722e58c
Sequence-10 <habz3963@gmail.com>
2023-07-19 06:28:52 -0700

    C-Language Module "Funtion Pointers (Task 0)"

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Dealing with function pointers that will print names
+ * @name: holds the name to be printed
+ * @f: the main worker
+ * Return: empty output
+ */
+
+void print_name(char *name, void(*void (*f)(char *))
+		{
+		if (f && name)
+			(*f)(name);
+		}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Alx SE Online Program daily exercise on C-Language Module at Foundation level "Function-Pointers-Project(19-7-2023)"
