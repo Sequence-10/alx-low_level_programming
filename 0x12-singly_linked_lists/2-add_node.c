38fc5862606d1ba6e3cc5eb81a2e90898216f6f5
Sequence-10 <habz3963@gmail.com>
2023-07-29 16:57:14 -0700

    Alx SE daily task on C-Language Module foundation level "singly-linked-list (task-2)"

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,37 +2,37 @@
 #include <string.h>
 
 /**
- * *add_node - a node to a linked list head
+ * *add_node - adds a node to a linked list head
  * @head: linked list head
  * @str: string element of the linked list
- * Return: exact number
+ * Return: number of elements
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *ben;
+	list_t *new_node;
 	int len_str;
 
-	ben = (list_t *) malloc(sizeof(list_t));
+	new_node = (list_t *) malloc(sizeof(list_t));
 
-	if (ben == NULL)
+	if (new_node == NULL)
 	{
-		free(ben);
+		free(new_node);
 		return (NULL);
 	}
 
-	ben->str = strdup(str);
-	if (ben->str == NULL)
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		free(ben);
+		free(new_node);
 		return (NULL);
 	}
 	for (len_str = 0; str[len_str] != '\0';  len_str++)
 		;
 
-	ben->len = len_str;
-	ben->next = *head;
-	*head = ben;
+	new_node->len = len_str;
+	new_node->next = *head;
+	*head = new_node;
 
-	return (ben);
+	return (new_node);
 }
