4dcda99a7417953669d3687679f12277c03b4f79
Sequence-10 <habz3963@gmail.com>
2023-07-29 17:05:36 -0700

    Alx SE daily task on C-Language Module foundation level "singly-linked-list (task-2)"

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,37 +2,27 @@
 #include <string.h>
 
 /**
- * *add_node - adds a node to a linked list head
- * @head: linked list head
- * @str: string element of the linked list
- * Return: number of elements
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to a pointer to the first node
+ * @str: string to be duplicated
+ *
+ * Return: the address of the new element, or NULL if it failed
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	int len_str;
 
-	new_node = (list_t *) malloc(sizeof(list_t));
-
-	if (new_node == NULL)
-	{
-		free(new_node);
+	if (head == NULL)
 		return (NULL);
-	}
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	for (len_str = 0; str[len_str] != '\0';  len_str++)
-		;
-
-	new_node->len = len_str;
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
