127d1fff765f633305eec677c9d3ba7c0516248a
Sequence-10 <habz3963@gmail.com>
2023-07-30 19:51:43 -0700

    Alx SE daily task on C-Language Module foundation level "Structures-Typedef"

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,22 +8,12 @@
  * @owner: the dog's onwer's name
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * struct dog dog_t - typedef for struct dog
- * Return: success
- */
-
-typedef struct dog dog_t
-{
-	;
-}
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
