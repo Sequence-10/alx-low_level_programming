44094365b86a75412a311f0364fe11822a741d8d
Sequence-10 <habz3963@gmail.com>
2023-07-30 21:19:06 -0700

    Alx SE daily task on C-Language Module Foundation Level "singly_linked_lists"

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,21 +8,22 @@
  *
  * Return: the address of the new element, or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new;
 
-	if (head == NULL)
-		return (NULL);
+	new = (list_t *) malloc(sizeof(list_t));
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
 
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
-
-	return (*head);
+	return (new);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#ifndef LIST_H
+#define LIST_H
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list list
- * @str: first variable 
+ * @str: first variable
  * @len: to know the lenght
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
 	char *str;
@@ -18,3 +25,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
+
+#endif
