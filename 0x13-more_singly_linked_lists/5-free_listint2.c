436e36aec89aae9fb0991e5b82b2fa753c76633c
Sequence-10 <habz3963@gmail.com>
2023-08-01 15:12:38 -0700

    C-More Singly Linked Lists Task-6 (Free)

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * free_listint2 - increment the new node to the begiining of the list
+ * @head: variable one
+ *
+ * Return: success
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint *runner;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		runner = *head;
+		*head = (*head)->next;
+		free(runner);
+	}
+}
