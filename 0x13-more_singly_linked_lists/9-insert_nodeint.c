a8b74407f48c1a2fa0a858319f563b076815ee90
Sequence-10 <habz3963@gmail.com>
2023-08-01 16:04:15 -0700

    C-More Singly Linked Lists Task-10/15 (Insert)

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * insert_nodeint_at_index -  nsert the new node
+ * @head: variable one
+ * @idx: variable two
+ * @n: last variable
+ *
+ * Return: success
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint *runner;
+	listint *new;
+
+	if (head == NULL)
+		return (0);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+
+	new->next = NULL;
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		(*head) = new;
+		return (new);
+	}
+
+	runner = *head;
+
+	while (idx != 1)
+	{
+		runner = runner->next;
+		-idx;
+
+		if (ruuner == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->next = current->next;
+	runner->next = new;
+
+	return (new);
+}
