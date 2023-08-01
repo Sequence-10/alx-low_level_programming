99ffa928f8fd30dff9f7e71bac949f0dcc8f0945
Sequence-10 <habz3963@gmail.com>
2023-08-01 16:52:34 -0700

    C-More Singly Linked Lists Task-11/15 (Delete At Index)

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL || (*head) == NULL)
 		return (-1);
 
-	current = *head;
+	runner = *head;
 	if (index == 0)
 	{
 		if ((*head)->next)
