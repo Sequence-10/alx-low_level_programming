dc70d73d2fb3e57f9d1381892b35ef7753617547
Sequence-10 <habz3963@gmail.com>
2023-08-01 16:48:46 -0700

    C-More Singly Linked Lists Task-11/15 (Delete At Index)

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * delete_nodeint_at_index -  nsert the new node
+ * @head: variable one
+ * @index: variable two
+ *
+ * Return: success
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *runner;
+	listint_t *tmp;
+
+	if (head == NULL || (*head) == NULL)
+		return (-1);
+
+	current = *head;
+	if (index == 0)
+	{
+		if ((*head)->next)
+			(*head) = (*head)->next;
+		else
+			(*head) = NULL;
+		free(runner);
+		return (1);
+	}
+
+	while (index != 1)
+	{
+		if (runner->next == NUL)
+			return (-1);
+
+		runner = runner->next;
+		--index;
+	}
+	tmp = runner->next;
+	if (runner->next)
+		runner->next = runner->next->next;
+	else
+		runner->next = NULL;
+	free(tmp);
+
+	return (1);
+}
