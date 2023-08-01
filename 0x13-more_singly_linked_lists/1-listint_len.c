767e114a17e515ff9fbce73bde9f34ff84d55682
Sequence-10 <habz3963@gmail.com>
2023-08-01 11:52:43 -0700

    C-More Singly Linked Lists Task-2 (List Length)

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * listint_len - give out the number of element in the linked list
+ * @h: variabel for the task
+ *
+ * Return: success
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nbr_member = 0;
+	const listint_t *runner = h;
+
+	while (runner != NULL)
+	{
+		runner = runner->nest;
+		nbr_member++
+	}
+	return (nbr_member);
+}
