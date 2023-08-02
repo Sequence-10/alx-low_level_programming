60a6dbe72d228aa2b4fa7c4ce3028c8e16fe231f
Sequence-10 <habz3963@gmail.com>
2023-08-01 17:51:45 -0700

    C-More Singly Linked Lists Task-15/15 (Find The Loop)

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *find_listint_loop -  nsert the new node
+ * @head: variable one
+ *
+ * Return: success
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+
+	return (NULL);
+}
