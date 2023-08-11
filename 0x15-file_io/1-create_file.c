5ac9c09b25a7bb52cbbe88316e71ee81c19aa2e7
Sequence-10 <habz3963@gmail.com>
2023-08-10 18:58:19 -0700

    Alx SE daily task on C-Language Module foundation level "File I/O (task-1)"

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - A function that creates a file
+ * @filename: Holds the name of the file to be created
+ * @text_content: direct it to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int a, b, c;
+
+	if (filename == NULL)
+		return (-1);
+
+	a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (a == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (c = 0; text_content[c] != '\0'; c++)
+			;
+		b = write(a, text_content, c);
+		if (b == -1)
+			return (-1);
+	}
+
+	clode(a);
+
+	return (1);
+}
