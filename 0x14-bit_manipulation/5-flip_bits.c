a528f7bd997756412b91897c91ef84c8e64a2edb
Sequence-10 <habz3963@gmail.com>
2023-08-26 10:56:35 -0700

    Alx SE Intensive Online Program at Foundation level on C Language "Bit Manipulation"

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - a function that returns the num of bits
+ * @n: player 1
+ * @m: players 2
+ * Return: flipped bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int coins;
+	unsigned int i = 0;
+
+	coins = n ^ m;
+	while (coins > 0)
+	{
+		i++;
+		coins &= (coins - 1);
+	}
+	return (i);
+}
