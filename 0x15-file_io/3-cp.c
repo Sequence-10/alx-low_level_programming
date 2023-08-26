647db3926f1baa2e60472f719de09a62757e89dc
Sequence-10 <habz3963@gmail.com>
2023-08-26 11:58:36 -0700

    Alx SE online program on foundation level "File I/O"

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,54 +1,67 @@
 #include "main.h"
 
-#define MAXSIZE 1024
-#define SE STDERR_FILENO
-
+void print_IO_stat(int stat, int fd, char *filename, char mode);
 /**
- * main - A program that couples the content of a file to another file
- * @ac: taking of the steps
- * @av: records
- *
- * Return: 0
+ * main - a function that copies the content of one file to another
+ * @argc: control the process
+ * @argv: serve as a backup
+ * Return: 1 on success, exit otherwise
  */
 
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
-	int a, b, c, d;
-	char buf[MAXSIZE];
-	mode_t mode;
+	int i, j, val_q = 1024, wrote, close_i, close_j;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
 
-	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-	if (ac != 3)
-		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
-	a = open(av[1], O_RDONLY);
-	if (a == -1)
-		dprintf(SE, "Error: can't read from file %s\n", av[1]), exit(98);
-
-	b = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
-	if (b == -1)
-		dprintf(SE, "Error: can't write to %s\n", av[2]), exit(99);
-	do {
-		c = read(a, buf, MAXSIZE);
-		if (c == -1)
-		{
-			dprintf(SE, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-
-		if (c > 0)
-		{
-			d = write(b, buf, (ssize_t) c);
-			if (d == -1)
-				dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
-		}
-	} while (c > 0);
-
-	c = close(a);
-	if (c == -1)
-		dprintf(SE, "Error: Can't close fd %d\n", a), exit(100);
-	d = close(b);
-	if (d == -1)
-		dprintf(SE, "Error: Can't close fd %d\n", b), exit(100);
-
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	i = open(argv[1], O_RDONLY);
+	print_IO_stat(i, -1, argv[1], 'O');
+	j = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	print_IO_stat(j, -1, argv[2], 'W');
+	while (val_q == 1024)
+	{
+		val_q = read(i, buffer, sizeof(buffer));
+		if (val_q == -1)
+			print_IO_stat(-1, -1, argv[1], 'O');
+		wrote = write(j, buffer, val_q);
+		if (wrote == -1)
+			print_IO_stat(-1, -1, argv[2], 'W');
+	}
+	close_i = close(i);
+	print_IO_stat(close_i, i, NULL, 'C');
+	close_j = close(j);
+	print_IO_stat(close_j, j, NULL, 'C');
 	return (0);
+}
+
+/**
+ * print_IO_stat - A function that checks if a file can be opened or closed
+ * @stat: the  descriptor of the file
+ * @filename: the name of the file
+ * @mode: closing or opening
+ * @fd: file descriptor
+ * Return: nothing
+ */
+void print_IO_stat(int stat, int fd, char *filename, char mode)
+{
+	if (mode == 'C' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	else if (mode == 'O' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	else if (mode == 'W' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
