e4309a50aca76fe09feb51aad52a1af331069d9a
sequence-10 <habz3963@gmail.com>
2023-09-08 16:36:01 -0700

    Solution of the daily task on c language at the foundation level "File I/O project 15 in month 3"

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define ARGUMENT_COUNT_EXPECTED 3
+#define FILE_ERROR -1
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +9,8 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <elf.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
